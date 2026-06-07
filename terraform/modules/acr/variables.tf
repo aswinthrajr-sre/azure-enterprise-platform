0264066ba472e3ea679a20b739f89c2453c6763a
Aswinthraj R <raswinthraj@gmail.com>
2026-06-07 23:13:55 +0530

    Add ACR Module

---
+++ terraform/modules/acr/variables.tf
@@ -0,0 +1,20 @@
+variable "acr_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "sku" {
+  type = string
+  default = "Standard"
+}
+
+variable "tags" {
+  type = map(string)
+}
