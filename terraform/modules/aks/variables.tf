6fde5918905a11ae38c2034f3e9d861a83ffdb00
Aswinthraj R <raswinthraj@gmail.com>
2026-06-07 23:16:07 +0530

    Add AKS Module

---
+++ terraform/modules/aks/variables.tf
@@ -0,0 +1,33 @@
+variable "aks_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "dns_prefix" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+  default = 2
+}
+
+variable "vm_size" {
+  type = string
+  default = "Standard_B2s"
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
