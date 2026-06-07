4a8e9c4726d21d2c365d4c2d830160af9196a978
Aswinthraj R <raswinthraj@gmail.com>
2026-06-07 23:12:00 +0530

    Add Keyvault Module

---
+++ terraform/modules/keyvault/variables.tf
@@ -0,0 +1,19 @@
+variable "keyvault_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
