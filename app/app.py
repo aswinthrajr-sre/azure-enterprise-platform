4a0a1d1bf65bdebcb2c14ef19799007ef956a11b
Aswinthraj R <raswinthraj@gmail.com>
2026-06-08 13:24:48 +0530

    Chnage the Txt

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from AKS and ACR!"
+    return "Hello State Street Team"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
